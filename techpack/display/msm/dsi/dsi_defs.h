3d0bfd72444d7068ea2db778ee5eb42ae3cc934a
Christos Athanasiadis <christos.ath4@gmail.com>
2023-11-09 21:27:40 +0200

    Revert "techpack: display: Kill Pixelworks Iris entirely"
    This reverts commit 6c2fc7ac9dbbffb5f69f9e1a048549290830e9c9.

--- techpack/display/msm/dsi/dsi_defs.h
+++ techpack/display/msm/dsi/dsi_defs.h
@@ -390,6 +390,9 @@ enum dsi_cmd_set_type {
 	DSI_CMD_DIMMING_GAMMA,
 	DSI_CMD_SET_FPS60,
 	DSI_CMD_SET_FPS120,
+#if defined(OPLUS_FEATURE_PXLW_IRIS5)
+	DSI_CMD_SET_ABYP,
+#endif
 #endif
 
 #ifdef OPLUS_FEATURE_ADFR
